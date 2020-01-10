3f3e76d7487071d34717e64bbc89abd2e30958dc
binarathambugala <binaraudeepa@gmail.com>
2020-01-10 10:24:50 +0530

    Add images to repository

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,24 @@ import { CarService } from "./services/car.service";
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+   styles: [`
+        :host ::ng-deep .drag-column {
+            padding-right: .5em;
+        }
+
+        :host ::ng-deep .drop-column {
+            border: 1px solid #c8c8c8;
+            background-color: #ffffff;
+        }
+
+        .ui-g li {
+            list-style-type: none;
+            padding: 10px;
+            margin-bottom: 5px;
+            border: 1px solid #c8c8c8;
+            background-color: #ffffff;
+        }
+    `]
 })
 export class AppComponent {
   availableCars: Car[];
@@ -18,7 +35,20 @@ export class AppComponent {
 
   ngOnInit() {
     this.selectedCars = [];
-    this.carService.getCarsSmall().then(cars => (this.availableCars = cars));
+
+    this.availableCars = [
+      { brand: "VW", year: 2012, color: "Orange", vin: "dsad231ff" },
+      { brand: "Audi", year: 2011, color: "Black", vin: "gwregre345" },
+      { brand: "Renault", year: 2005, color: "Gray", vin: "h354htr" },
+      { brand: "BMW", year: 2003, color: "Blue", vin: "j6w54qgh" },
+      { brand: "Mercedes", year: 1995, color: "Orange", vin: "hrtwy34" },
+      { brand: "Volvo", year: 2005, color: "Black", vin: "jejtyj" },
+      { brand: "Honda", year: 2012, color: "Yellow", vin: "g43gr" },
+      { brand: "Jaguar", year: 2013, color: "Orange", vin: "greg34" },
+      { brand: "Ford", year: 2000, color: "Black", vin: "h54hw5" },
+      { brand: "Fiat", year: 2013, color: "Red", vin: "245t2s" }
+    ];
+    // this.carService.getCarsSmall().then(cars => (this.availableCars = cars));
   }
 
   dragStart(event, car: Car) {
