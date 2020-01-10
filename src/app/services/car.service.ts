2e498cb745e24c7c2e4f8dbcd5606c712da13fe8
binarathambugala <binaraudeepa@gmail.com>
2020-01-10 09:09:28 +0530

    2 files modified

--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -9,28 +9,28 @@ export class CarService {
     constructor(private http: HttpClient) { }
 
     getCarsSmall() {
-    return this.http.get<any>('assets/showcase/data/cars-small.json')
+    return this.http.get<any>('../assets/data/cars-small.json')
       .toPromise()
       .then(res => <Car[]>res.data)
       .then(data => { return data; });
     }
 
     getCarsMedium() {
-    return this.http.get<any>('assets/showcase/data/cars-medium.json')
+    return this.http.get<any>('../assets/data/cars-medium.json')
       .toPromise()
       .then(res => <Car[]>res.data)
       .then(data => { return data; });
     }
 
     getCarsLarge() {
-    return this.http.get<any>('assets/showcase/data/cars-large.json')
+    return this.http.get<any>('../assets/data/cars-large.json')
       .toPromise()
       .then(res => <Car[]>res.data)
       .then(data => { return data; });
     }
 
   getCarsHuge() {
-    return this.http.get<any>('assets/showcase/data/cars-huge.json')
+    return this.http.get<any>('../assets/data/cars-huge.json')
       .toPromise()
       .then(res => <Car[]>res.data)
       .then(data => { return data; });
